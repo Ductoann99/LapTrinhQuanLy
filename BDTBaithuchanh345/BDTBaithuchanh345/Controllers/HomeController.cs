67b1897cb38d2c3a3aabcb6594527d77ad18ac86
Ductoann99 <89242254+Ductoann99@users.noreply.github.com>
2022-01-19 10:19:10 +0700

    Commit_Note

--- BDTBaithuchanh345/BDTBaithuchanh345/Controllers/HomeController.cs
+++ BDTBaithuchanh345/BDTBaithuchanh345/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace BDTBaithuchanh345.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "helooo.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "heloooooooooo.";
 
             return View();
         }
