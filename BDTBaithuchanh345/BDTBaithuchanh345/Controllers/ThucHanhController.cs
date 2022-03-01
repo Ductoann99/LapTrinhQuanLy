126efec39ce1063376dc1727a6ae62825a4a8be4
Ductoann99 <89242254+Ductoann99@users.noreply.github.com>
2022-03-01 10:35:18 +0700

    Commit_Note

---
+++ BDTBaithuchanh345/BDTBaithuchanh345/Controllers/ThucHanhController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BDTBaithuchanh345.Controllers
+{
+    public class ThucHanhController : Controller
+    {
+        // GET: ThucHanh
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult About(string Masinhvien , string Hovaten)
+        {
+            ViewBag.Message =  Masinhvien + " " + Hovaten;
+
+            return View();
+        }
+       
+    }
+}
